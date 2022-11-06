1e1051527b6747016233a4576912d44c6b494c9d
thomas2392 <thomas.ribeiro2392@gmail.com>
2022-11-06 07:54:09 -0300

    FeignClient implementation

--- hr-payroll/src/main/java/com/thomas/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/thomas/hrpayroll/services/PaymentService.java
@@ -1,31 +1,20 @@
 package com.thomas.hrpayroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.thomas.hrpayroll.entities.Payment;
 import com.thomas.hrpayroll.entities.Worker;
+import com.thomas.hrpayroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
 
 	@Autowired
-	private RestTemplate restTemplate;
-
-	@Value("${hr-worker.host}")
-	private String workerHost;
+	private WorkerFeignClient feignClient;
 
 	public Payment getPayment(long workerId, int days) {
-		Map<String, String> uriVariables = new HashMap<>();
-		uriVariables.put("id", ""+workerId);
-		String url = workerHost + "/workers/{id}";
-		Worker worker = restTemplate.getForObject(url, Worker.class, uriVariables);
-
+		Worker worker = feignClient.findById(workerId).getBody();
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
